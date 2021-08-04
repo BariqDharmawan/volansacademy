eafa570c053d51afe60e514037d081c9698f2416
BariqDharmawan <bariq.2nd.rodriguez@gmail.com>
2021-08-04 13:26:34 +0700

    [UPDATE] gitignore

--- database/factories/AdvantageFactory.php
+++ database/factories/AdvantageFactory.php
@@ -8,6 +8,6 @@ use Faker\Generator as Faker;
 $factory->define(Advantage::class, function (Faker $faker) {
     return [
         'name' => $faker->sentence(mt_rand(4, 6)),
-        'image' => $faker->randomElement(['study.jpg', 'library.jpg'])
+        'image' => $faker->image('public/class', 640,480, 'library', false)
     ];
 });
--- database/factories/BlogFactory.php
+++ database/factories/BlogFactory.php
@@ -9,7 +9,7 @@ $factory->define(Blog::class, function (Faker $faker) {
     return [
         'title' => $faker->sentence($nbWords = 4, $variableNbWords = true),
         'body' => $faker->text(200),
-        'featured_image' => $faker->randomElement(['1.jpg', '2.jpg']),
+        'featured_image' => $faker->image('public/class', 640,480, 'laptop', false),
         'short_description' => $faker->text(20)
     ];
 });
