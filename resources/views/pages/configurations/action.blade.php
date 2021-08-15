1d29d9ca1693a68d90e61cd097d6b2f1550cf617
BariqDharmawan <bariq.2nd.rodriguez@gmail.com>
2021-08-15 19:13:03 +0700

    [UPDATE] remove configuration feature

--- resources/views/pages/configurations/action.blade.php
+++
@@ -1,2 +0,0 @@
-<a class="btn btn-info pt-0 pb-0" href="{{ route('configurations.show',$configuration->id) }}">Detail</a>
-<a class="btn btn-primary pt-0 pb-0" href="{{ route('configurations.edit',$configuration->id) }}">Edit</a>
