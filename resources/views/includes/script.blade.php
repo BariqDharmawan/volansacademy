7ecd0514a90f39254004201175359ac35abc6e91
BariqDharmawan <bariq.2nd.rodriguez@gmail.com>
2021-08-16 17:18:21 +0700

    [UPDATE] script includes

--- resources/views/includes/script.blade.php
+++ resources/views/includes/script.blade.php
@@ -1,35 +1,24 @@
-<!-- Scripts -->
-<!-- REQUIRED SCRIPTS -->
-<!-- jQuery -->
-<script src="{{ asset('AdminLTE/plugins/jquery/jquery.min.js') }}"></script>
-<!-- Bootstrap -->
-<script src="{{ asset('AdminLTE/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-<!-- overlayScrollbars -->
-<script src="{{ asset('AdminLTE/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js') }}"></script>
+@php
+    $assets = [
+        'jquery/jquery.min.js',
+        'bootstrap/js/bootstrap.bundle.min.js',
+        'overlayScrollbars/js/jquery.overlayScrollbars.min.js',
+        'datatables/jquery.dataTables.min.js',
+        'datatables-bs4/js/dataTables.bootstrap4.min.js',
+        'datatables-responsive/js/dataTables.responsive.min.js',
+        'datatables-responsive/js/responsive.bootstrap4.min.js',
+        'select2/js/select2.full.min.js',
+        'moment/moment.min.js',
+        'inputmask/min/jquery.inputmask.bundle.min.js',
+        'daterangepicker/daterangepicker.js',
+        'tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js',
+        'croppie/croppie.js',
+    ];
+@endphp
 
-<script src="{{ asset('AdminLTE/plugins/datatables/jquery.dataTables.min.js') }}"></script>
-<script src="{{ asset('AdminLTE/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
-<script src="{{ asset('AdminLTE/plugins/datatables-responsive/js/dataTables.responsive.min.js') }}"></script>
-<script src="{{ asset('AdminLTE/plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}"></script>
+@foreach ($assets as $asset)
+<script src="{{ asset('AdminLTE/plugins/' . $asset) }}"></script>
+@endforeach
 
-<!-- Select2 -->
-<script src="{{ asset('AdminLTE/plugins/select2/js/select2.full.min.js') }}"></script>
-
-<script src="{{ asset('AdminLTE/plugins/moment/moment.min.js') }}"></script>
-<script src="{{ asset('AdminLTE/plugins/inputmask/min/jquery.inputmask.bundle.min.js') }}"></script>
-
-<!-- date-range-picker -->
-<script src="{{ asset('AdminLTE/plugins/daterangepicker/daterangepicker.js') }}"></script>
-
-<!-- Tempusdominus Bootstrap 4 -->
-<script src="{{ asset('AdminLTE/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js') }}"></script>
-
-<!-- Croppie -->
-<script src="{{ asset('AdminLTE/plugins/croppie/croppie.js') }}"></script>
-
-<!-- AdminLTE App -->
 <script src="{{ asset('AdminLTE/dist/js/adminlte.js') }}"></script>
-
-<!-- OPTIONAL SCRIPTS -->
 <script src="{{ asset('AdminLTE/dist/js/demo.js') }}"></script>
-
